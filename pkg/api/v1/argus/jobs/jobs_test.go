950132f1a0b46592c19feaf9854b597b22167724
Dean <deanoren@gmail.com>
2022-10-14 09:47:57 +0200

    add wait for argus jobs (#27)
    * ship wait function with better timeout for create's wait
    
    * add wait service to jobs create and delete + unit tests
    
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

--- pkg/api/v1/argus/jobs/jobs_test.go
+++ pkg/api/v1/argus/jobs/jobs_test.go
@@ -7,10 +7,12 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
+	"time"
 
-	"github.com/SchwarzIT/community-stackit-go-client"
+	client "github.com/SchwarzIT/community-stackit-go-client"
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/api/v1/argus/jobs"
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/consts"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/wait"
 )
 
 // constants
@@ -19,7 +21,7 @@ const (
 	apiPathJob = "/%s"
 )
 
-func prep(t *testing.T, path, projectID, instanceID, want, method string) (*jobs.JobsService, func()) {
+func prep(t *testing.T, path, projectID, instanceID, want, method string) (*jobs.JobsService, *http.ServeMux, func()) {
 	c, mux, teardown, _ := client.MockServer()
 	a := jobs.New(c)
 
@@ -32,13 +34,13 @@ func prep(t *testing.T, path, projectID, instanceID, want, method string) (*jobs
 		fmt.Fprint(w, string(want))
 	})
 
-	return a, teardown
+	return a, mux, teardown
 }
 
 func TestJobsService_List(t *testing.T) {
 	pid := "1234"
 	iid := "5678"
-	a, teardown := prep(t, "", pid, iid, list_response, http.MethodGet)
+	a, _, teardown := prep(t, "", pid, iid, list_response, http.MethodGet)
 	defer teardown()
 
 	var want jobs.ListJobsResponse
@@ -101,7 +103,7 @@ func TestJobsService_Get(t *testing.T) {
 	pid := "1234"
 	iid := "5678"
 	name := "job-name-1"
-	a, teardown := prep(t, fmt.Sprintf(apiPathJob, name), pid, iid, get_response, http.MethodGet)
+	a, _, teardown := prep(t, fmt.Sprintf(apiPathJob, name), pid, iid, get_response, http.MethodGet)
 	defer teardown()
 
 	var want jobs.GetJobResponse
@@ -159,8 +161,36 @@ const get_response = `{
 func TestJobsService_Create(t *testing.T) {
 	pid := "1234"
 	iid := "5678"
-	a, teardown := prep(t, "", pid, iid, create_response, http.MethodPost)
+	a, mux, teardown := prep(t, "", pid, iid, create_response, http.MethodPost)
 	defer teardown()
+
+	baseDuration := 200 * time.Millisecond
+	ctx1, td1 := context.WithTimeout(context.Background(), 1*baseDuration)
+	defer td1()
+
+	ctx2, td2 := context.WithTimeout(context.Background(), 2*baseDuration)
+	defer td2()
+
+	mux.HandleFunc(fmt.Sprintf(apiPath+apiPathJob, pid, iid, "my-job"), func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			t.Error("wrong method")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		if ctx1.Err() == nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if ctx2.Err() == nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, get_response)
+	})
 
 	var want jobs.ListJobsResponse
 	if err := json.Unmarshal([]byte(create_response), &want); err != nil {
@@ -197,14 +227,17 @@ func TestJobsService_Create(t *testing.T) {
 		wantRes jobs.ListJobsResponse
 		wantErr bool
 		compare bool
+		useWait bool
 	}{
-		{"all ok", args{context.Background(), pid, iid, job}, want, false, true},
-		{"bad job", args{context.Background(), pid, iid, job2}, want, true, false},
-		{"nil ctx", args{nil, pid, iid, job}, want, true, false},
+		{"all ok", args{context.Background(), pid, iid, job}, want, false, true, true},
+		{"bad job", args{context.Background(), pid, iid, job2}, want, true, false, false},
+		{"nil ctx", args{nil, pid, iid, job}, want, true, false, false},
 	}
+
+	var process *wait.Handler
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotRes, err := a.Create(tt.args.ctx, tt.args.projectID, tt.args.instanceID, tt.args.job)
+			gotRes, w, err := a.Create(tt.args.ctx, tt.args.projectID, tt.args.instanceID, tt.args.job)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("JobsService.Create() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -212,7 +245,22 @@ func TestJobsService_Create(t *testing.T) {
 			if !reflect.DeepEqual(gotRes, tt.wantRes) && tt.compare {
 				t.Errorf("JobsService.Create() = %v, want %v", gotRes, tt.wantRes)
 			}
+			if tt.useWait {
+				process = w
+			}
 		})
+	}
+
+	process.SetThrottle(baseDuration)
+	// first test run: expect Wait to exit with an error
+	if _, err := process.Wait(); err == nil {
+		t.Error("expected error but got nil")
+	}
+
+	time.Sleep(baseDuration)
+	// 2nd test: expect Not found, and exit with success after retry
+	if _, err := process.Wait(); err != nil {
+		t.Errorf("expected no error but got: %v", err)
 	}
 }
 
@@ -239,7 +287,7 @@ const create_response = `{
 func TestJobsService_Update(t *testing.T) {
 	pid := "1234"
 	iid := "5678"
-	a, teardown := prep(t, "/my-job", pid, iid, create_response, http.MethodPut)
+	a, _, teardown := prep(t, "/my-job", pid, iid, create_response, http.MethodPut)
 	defer teardown()
 
 	var want jobs.ListJobsResponse
@@ -300,8 +348,43 @@ func TestJobsService_Delete(t *testing.T) {
 	pid := "1234"
 	iid := "5678"
 	jid := "my-job"
-	a, teardown := prep(t, "/"+jid, pid, iid, delete_response, http.MethodDelete)
+
+	c, mux, teardown, _ := client.MockServer()
+	a := jobs.New(c)
 	defer teardown()
+
+	baseDuration := 200 * time.Millisecond
+	ctx1, td1 := context.WithTimeout(context.Background(), 1*baseDuration)
+	defer td1()
+
+	ctx2, td2 := context.WithTimeout(context.Background(), 2*baseDuration)
+	defer td2()
+
+	mux.HandleFunc(fmt.Sprintf(apiPath+apiPathJob, pid, iid, jid), func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodDelete {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, delete_response)
+			return
+		}
+
+		if r.Method != http.MethodGet {
+			t.Error("wrong method")
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		if ctx1.Err() == nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if ctx2.Err() == nil {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, get_response)
+			return
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+	})
 
 	var want jobs.ListJobsResponse
 	if err := json.Unmarshal([]byte(delete_response), &want); err != nil {
@@ -320,14 +403,16 @@ func TestJobsService_Delete(t *testing.T) {
 		wantRes jobs.ListJobsResponse
 		wantErr bool
 		compare bool
+		useWait bool
 	}{
-		{"all ok", args{context.Background(), pid, iid, jid}, want, false, true},
-		{"bad job", args{context.Background(), pid, iid, "something"}, want, true, false},
-		{"nil ctx", args{nil, pid, iid, jid}, want, true, false},
+		{"all ok", args{context.Background(), pid, iid, jid}, want, false, true, true},
+		{"bad job", args{context.Background(), pid, iid, "something"}, want, true, false, false},
+		{"nil ctx", args{nil, pid, iid, jid}, want, true, false, false},
 	}
+	var process *wait.Handler
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotRes, err := a.Delete(tt.args.ctx, tt.args.projectID, tt.args.instanceID, tt.args.jobName)
+			gotRes, w, err := a.Delete(tt.args.ctx, tt.args.projectID, tt.args.instanceID, tt.args.jobName)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("JobsService.Delete() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -335,7 +420,22 @@ func TestJobsService_Delete(t *testing.T) {
 			if !reflect.DeepEqual(gotRes, tt.wantRes) && tt.compare {
 				t.Errorf("JobsService.Delete() = %v, want %v", gotRes, tt.wantRes)
 			}
+			if tt.useWait {
+				process = w
+			}
 		})
+	}
+
+	process.SetThrottle(baseDuration)
+	// first test run: expect Wait to exit with an error
+	if _, err := process.Wait(); err == nil {
+		t.Error("expected error but got nil")
+	}
+
+	time.Sleep(baseDuration)
+	// 2nd test: expect found, retrey & not found -> exist successfully
+	if _, err := process.Wait(); err != nil {
+		t.Errorf("expected no error but got: %v", err)
 	}
 }
 
