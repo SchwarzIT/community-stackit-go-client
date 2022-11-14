136ce67b76ec28b86603680579981f8a9010f10a
Dean Oren <deangili.oren@mail.schwarz>
2022-11-14 09:41:37 +0100

    fix test

--- pkg/api/v1/postgres-flex/users/users_test.go
+++ pkg/api/v1/postgres-flex/users/users_test.go
@@ -18,7 +18,7 @@ import (
 // constants
 const (
 	apiPathList   = consts.API_PATH_POSTGRES_FLEX_USERS
-	apiPathCreate = consts.API_PATH_POSTGRES_FLEX_USER
+	apiPathCreate = consts.API_PATH_POSTGRES_FLEX_USERS
 	apiPathGet    = consts.API_PATH_POSTGRES_FLEX_USER
 )
 
@@ -148,7 +148,7 @@ func TestPostgresUsersService_Create(t *testing.T) {
 	instanceID := "efg"
 	userID := "123"
 
-	mux.HandleFunc(fmt.Sprintf(apiPathGet, projectID, instanceID, userID), func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(fmt.Sprintf(apiPathCreate, projectID, instanceID), func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			t.Error("wrong method")
 		}
@@ -174,7 +174,6 @@ func TestPostgresUsersService_Create(t *testing.T) {
 		ctx        context.Context
 		projectID  string
 		instanceID string
-		userID     string
 		username   string
 		database   string
 		roles      []string
@@ -190,7 +189,6 @@ func TestPostgresUsersService_Create(t *testing.T) {
 			projectID,
 			instanceID,
 			userID,
-			"string",
 			"string",
 			[]string{"string"},
 		}, users.CreateResponse{
@@ -210,7 +208,7 @@ func TestPostgresUsersService_Create(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotRes, err := postgres.Users.Create(tt.args.ctx, tt.args.projectID, tt.args.instanceID, tt.args.userID, tt.args.username, tt.args.database, tt.args.roles)
+			gotRes, err := postgres.Users.Create(tt.args.ctx, tt.args.projectID, tt.args.instanceID, tt.args.username, tt.args.database, tt.args.roles)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("PostgresUsersService.Create() error = %v, wantErr %v", err, tt.wantErr)
 				return
