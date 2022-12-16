6cee3d8dcb098668bb050d3c49ba9bb9f79bb688
Dean <deanoren@gmail.com>
2022-12-16 15:52:11 +0100

    update makefile, config and generated code (#57)
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

--- pkg/services/kubernetes/v1.4/main.go
+++ pkg/services/kubernetes/v1.4/main.go
@@ -1,3 +1,3 @@
 package gen
 
-//go:generate go run github.com/do87/oapi-codegen/cmd/oapi-codegen@v0.2.0 -config config.yaml swagger.json
+//go:generate go run github.com/do87/oapi-codegen/cmd/oapi-codegen@latest -config config.yaml swagger.json
