7718b58efba7b444c4f3b56926456c7afbc97adc
Dean Oren <deangili.oren@mail.schwarz>
2022-12-20 15:14:58 +0100

    update service url

--- pkg/services/kubernetes/v1.4/generated/service.go
+++ pkg/services/kubernetes/v1.4/generated/service.go
@@ -1,16 +1,10 @@
 package kubernetes
 
 import (
-	"net/url"
-
 	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/consts"
 )
 
 func NewService(c common.Client) (*ClientWithResponses, error) {
-	surl, err := url.JoinPath(consts.DEFAULT_BASE_URL, consts.API_PATH_SKE)
-	if err != nil {
-		return nil, err
-	}
-	return NewClientWithResponses(surl, WithHTTPClient(c))
+	return NewClientWithResponses(consts.BASE_URL_SKE, WithHTTPClient(c))
 }
