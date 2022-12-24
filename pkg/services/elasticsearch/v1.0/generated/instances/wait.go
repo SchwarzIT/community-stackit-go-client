92828e466ec9e9e61a7eee88879d4ba922914c5d
Dean Oren <deangili.oren@mail.schwarz>
2022-12-24 15:20:17 +0100

    add elasticsearch

---
+++ pkg/services/elasticsearch/v1.0/generated/instances/wait.go
@@ -0,0 +1,87 @@
+package instances
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/wait"
+)
+
+const (
+	client_timeout_err = "Client.Timeout exceeded while awaiting headers"
+)
+
+func (r ProvisionResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, instanceID string) *wait.Handler {
+	return wait.New(func() (res interface{}, done bool, err error) {
+		s, err := c.GetWithResponse(ctx, projectID, instanceID)
+		if err != nil && !strings.Contains(err.Error(), client_timeout_err) {
+			return nil, false, err
+		}
+		if s.HasError != nil && !strings.Contains(err.Error(), client_timeout_err) {
+			return nil, false, err
+		}
+		if s.JSON200 == nil {
+			return nil, false, errors.New("bad response, JSON200 is nil")
+		}
+		if s.JSON200.LastOperation.State == SUCCEEDED {
+			return s, true, nil
+		}
+		if s.JSON200.LastOperation.State == FAILED {
+			return s, false, errors.New("received failed status from DSA instance")
+		}
+		return s, false, nil
+	})
+}
+
+func (r UpdateResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, instanceID string) *wait.Handler {
+	return wait.New(func() (res interface{}, done bool, err error) {
+		s, err := c.GetWithResponse(ctx, projectID, instanceID)
+		if err != nil && !strings.Contains(err.Error(), client_timeout_err) {
+			return nil, false, err
+		}
+		if s.HasError != nil && !strings.Contains(err.Error(), client_timeout_err) {
+			return nil, false, err
+		}
+		if s.JSON200 == nil {
+			return nil, false, errors.New("bad response, JSON200 is nil")
+		}
+		if s.JSON200.LastOperation.Type != UPDATE {
+			return s, false, nil
+		}
+		if s.JSON200.LastOperation.State == SUCCEEDED {
+			return s, true, nil
+		}
+		if s.JSON200.LastOperation.State == FAILED {
+			return s, false, errors.New("received failed status from DSA instance")
+		}
+		return s, false, nil
+	})
+}
+
+func (r DeprovisionResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, instanceID string) *wait.Handler {
+	return wait.New(func() (res interface{}, done bool, err error) {
+		s, err := c.GetWithResponse(ctx, projectID, instanceID)
+		if err != nil && !strings.Contains(err.Error(), client_timeout_err) {
+			if strings.Contains(err.Error(), http.StatusText(http.StatusNotFound)) ||
+				strings.Contains(err.Error(), http.StatusText(http.StatusGone)) {
+				return nil, true, nil
+			}
+			return s, false, err
+		}
+		if s.JSON200 == nil {
+			return nil, false, errors.New("bad response, JSON200 is nil")
+		}
+		if s.JSON200.LastOperation.Type != DELETE {
+			return s, false, nil
+		}
+		if s.JSON200.LastOperation.State == SUCCEEDED {
+			return s, true, nil
+		}
+		if s.JSON200.LastOperation.State == FAILED {
+			return s, false, errors.New("received failed status for DSA instance deletion")
+		}
+		return s, false, nil
+	})
+}
---
+++ pkg/services/elasticsearch/v1.0/include/instances/common.go
@@ -0,0 +1,18 @@
+// this file is only used to prevent wait.go
+// from showing errors
+
+package instances
+
+import "github.com/SchwarzIT/community-stackit-go-client/pkg/services/elasticsearch/v1.0/generated/instances"
+
+type ProvisionResponse struct {
+	instances.ClientWithResponsesInterface
+}
+
+type UpdateResponse struct {
+	instances.ClientWithResponsesInterface
+}
+
+type DeprovisionResponse struct {
+	instances.ClientWithResponsesInterface
+}
