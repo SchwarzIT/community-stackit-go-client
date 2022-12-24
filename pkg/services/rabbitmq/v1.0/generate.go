b58c9174d60c18255438ad1c5f18e6e3975f901d
Dean Oren <deangili.oren@mail.schwarz>
2022-12-24 15:12:14 +0100

    add rabbitmq

---
+++ pkg/services/rabbitmq/v1.0/generate.go
@@ -0,0 +1,3 @@
+package gen
+
+//go:generate go run github.com/do87/oapi-codegen/cmd/oapi-codegen@latest -config config.yaml rabbitmq.json
---
+++ pkg/services/rabbitmq/v1.0/generated/service.go
@@ -0,0 +1,16 @@
+package rabbitmq
+
+import (
+	"net/url"
+	"path"
+
+	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/consts"
+)
+
+func NewService(c common.Client) *ClientWithResponses {
+	u, _ := url.Parse(consts.DEFAULT_BASE_URL)
+	u.Path = path.Join(u.Path, consts.API_PATH_POSTGRES_FLEX)
+	nc, _ := NewClientWithResponses(u.String(), WithHTTPClient(c))
+	return nc
+}
