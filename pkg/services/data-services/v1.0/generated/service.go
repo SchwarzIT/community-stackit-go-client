be790b61259c882ed2fd290b884a99fe2285d47d
Dean <deanoren@gmail.com>
2022-12-28 19:23:01 +0100

    Fix data services URLs (#78)
    Add example
    
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

--- pkg/services/data-services/v1.0/generated/service.go
+++ pkg/services/data-services/v1.0/generated/service.go
@@ -1,9 +1,6 @@
 package dataservices
 
 import (
-	"net/url"
-	"path"
-
 	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/consts"
 )
@@ -19,25 +16,25 @@ const (
 )
 
 func NewService(c common.Client, serviceID int) *ClientWithResponses {
-	u, _ := url.Parse(consts.DEFAULT_BASE_URL)
+	url := consts.DEFAULT_BASE_URL
 
 	switch serviceID {
 	case ElasticSearch:
-		u.Path = path.Join(u.Path, "https://dsa-elasticsearch.api.eu01.stackit.cloud")
+		url = "https://dsa-elasticsearch.api.eu01.stackit.cloud"
 	case LogMe:
-		u.Path = path.Join(u.Path, "https://dsa-logme.api.eu01.stackit.cloud")
+		url = "https://dsa-logme.api.eu01.stackit.cloud"
 	case MariaDB:
-		u.Path = path.Join(u.Path, "https://dsa-mariadb.api.eu01.stackit.cloud")
+		url = "https://dsa-mariadb.api.eu01.stackit.cloud"
 	case MongoDB:
-		u.Path = path.Join(u.Path, "https://mongo-flex-prd.api.eu01.stackit.cloud")
+		url = "https://mongo-flex-prd.api.eu01.stackit.cloud"
 	case PostgresDB:
-		u.Path = path.Join(u.Path, "https://postgres-flex-service.api.eu01.stackit.cloud")
+		url = "https://postgres-flex-service.api.eu01.stackit.cloud"
 	case RabbitMQ:
-		u.Path = path.Join(u.Path, "https://rabbitmq.api.eu01.stackit.cloud")
+		url = "https://rabbitmq.api.eu01.stackit.cloud"
 	case Reddis:
-		u.Path = path.Join(u.Path, "https://redis.api.eu01.stackit.cloud")
+		url = "https://redis.api.eu01.stackit.cloud"
 	}
 
-	nc, _ := NewClientWithResponses(u.String(), WithHTTPClient(c))
+	nc, _ := NewClientWithResponses(url, WithHTTPClient(c))
 	return nc
 }
--- pkg/services/data-services/v1.0/include/service.go
+++ pkg/services/data-services/v1.0/include/service.go
@@ -1,9 +1,6 @@
 package dataservices
 
 import (
-	"net/url"
-	"path"
-
 	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/consts"
 	dataservices "github.com/SchwarzIT/community-stackit-go-client/pkg/services/data-services/v1.0/generated"
@@ -20,25 +17,25 @@ const (
 )
 
 func NewService(c common.Client, serviceID int) *dataservices.ClientWithResponses {
-	u, _ := url.Parse(consts.DEFAULT_BASE_URL)
+	url := consts.DEFAULT_BASE_URL
 
 	switch serviceID {
 	case ElasticSearch:
-		u.Path = path.Join(u.Path, "https://dsa-elasticsearch.api.eu01.stackit.cloud")
+		url = "https://dsa-elasticsearch.api.eu01.stackit.cloud"
 	case LogMe:
-		u.Path = path.Join(u.Path, "https://dsa-logme.api.eu01.stackit.cloud")
+		url = "https://dsa-logme.api.eu01.stackit.cloud"
 	case MariaDB:
-		u.Path = path.Join(u.Path, "https://dsa-mariadb.api.eu01.stackit.cloud")
+		url = "https://dsa-mariadb.api.eu01.stackit.cloud"
 	case MongoDB:
-		u.Path = path.Join(u.Path, "https://mongo-flex-prd.api.eu01.stackit.cloud")
+		url = "https://mongo-flex-prd.api.eu01.stackit.cloud"
 	case PostgresDB:
-		u.Path = path.Join(u.Path, "https://postgres-flex-service.api.eu01.stackit.cloud")
+		url = "https://postgres-flex-service.api.eu01.stackit.cloud"
 	case RabbitMQ:
-		u.Path = path.Join(u.Path, "https://rabbitmq.api.eu01.stackit.cloud")
+		url = "https://rabbitmq.api.eu01.stackit.cloud"
 	case Reddis:
-		u.Path = path.Join(u.Path, "https://redis.api.eu01.stackit.cloud")
+		url = "https://redis.api.eu01.stackit.cloud"
 	}
 
-	nc, _ := dataservices.NewClientWithResponses(u.String(), dataservices.WithHTTPClient(c))
+	nc, _ := dataservices.NewClientWithResponses(url, dataservices.WithHTTPClient(c))
 	return nc
 }
