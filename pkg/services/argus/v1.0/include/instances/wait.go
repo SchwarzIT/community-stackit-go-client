5e8cfdd92f60cb4e29090051c05b9ca6b2c92aa5
Dean Oren <deangili.oren@mail.schwarz>
2022-12-28 21:39:19 +0100

    test context deadline response error

--- pkg/services/argus/v1.0/include/instances/wait.go
+++ pkg/services/argus/v1.0/include/instances/wait.go
@@ -12,22 +12,18 @@ import (
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/wait"
 )
 
-const (
-	client_timeout_err = "Client.Timeout exceeded while awaiting headers"
-)
-
 // WaitHandler will wait for instance creation
 // returned interface is nil or *instances.ProjectInstanceUI
 func (r InstanceCreateResponse) WaitHandler(ctx context.Context, c *instances.ClientWithResponses, projectID, instanceID string) *wait.Handler {
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.InstanceReadWithResponse(ctx, projectID, instanceID)
 		if err != nil {
-			if !strings.Contains(err.Error(), client_timeout_err) {
-				return s, false, err
-			}
-			return nil, false, nil
+			return nil, false, err
 		}
 		if s.StatusCode() == http.StatusInternalServerError {
+			return nil, false, nil
+		}
+		if strings.Contains(s.HasError.Error(), "context deadline exceeded") {
 			return nil, false, nil
 		}
 		if s.HasError != nil {
@@ -49,12 +45,12 @@ func (r InstanceUpdateResponse) WaitHandler(ctx context.Context, c *instances.Cl
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.InstanceReadWithResponse(ctx, projectID, instanceID)
 		if err != nil {
-			if !strings.Contains(err.Error(), client_timeout_err) {
-				return s, false, err
-			}
-			return nil, false, nil
+			return nil, false, err
 		}
 		if s.StatusCode() == http.StatusInternalServerError {
+			return nil, false, nil
+		}
+		if strings.Contains(s.HasError.Error(), "context deadline exceeded") {
 			return nil, false, nil
 		}
 		if s.HasError != nil {
@@ -105,15 +101,15 @@ func (r InstanceDeleteResponse) WaitHandler(ctx context.Context, c *instances.Cl
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.InstanceReadWithResponse(ctx, projectID, instanceID)
 		if err != nil {
-			if !strings.Contains(err.Error(), client_timeout_err) {
-				return s, false, err
-			}
-			return nil, false, nil
+			return nil, false, err
 		}
 		if s.StatusCode() == http.StatusNotFound {
 			return nil, true, nil
 		}
 		if s.StatusCode() == http.StatusInternalServerError {
+			return nil, false, nil
+		}
+		if strings.Contains(s.HasError.Error(), "context deadline exceeded") {
 			return nil, false, nil
 		}
 		if s.HasError != nil {
