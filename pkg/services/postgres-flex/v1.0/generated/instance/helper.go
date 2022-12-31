ea7bf03778035758020d0e0dc0531111c33a5f79
Dean <deanoren@gmail.com>
2022-12-31 13:59:22 +0100

    cleanup consts (#85)
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

---
+++ pkg/services/postgres-flex/v1.0/generated/instance/helper.go
@@ -0,0 +1,9 @@
+package instance
+
+const (
+	// Instance status options
+	STATUS_READY      = "READY"
+	STATUS_FAILED     = "FAILED"
+	STATUS_PROCESSING = "PROCESSING"
+	STATUS_UNKNOWN    = "UNKNOWN"
+)
