34be58da0c411c203dd509ce68394cad6aa36fb1
Dean Oren <deangili.oren@mail.schwarz>
2023-01-04 19:42:15 +0100

    add user example

---
+++ examples/membership/adduser.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	client "github.com/SchwarzIT/community-stackit-go-client"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/consts"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/services/membership/v2.0/generated/membership"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/services/resource-management/v2.0/generated/projects"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/validate"
+)
+
+func main() {
+	ctx := context.Background()
+	c, err := client.New(ctx, client.Config{
+		ServiceAccountEmail: os.Getenv("STACKIT_SERVICE_ACCOUNT_EMAIL"),
+		ServiceAccountToken: os.Getenv("STACKIT_SERVICE_ACCOUNT_TOKEN"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	member := membership.Member{
+		Subject: "user@host.name",
+		Role:    string(projects.PROJECT_OWNER),
+	}
+	body := membership.AddMembersJSONRequestBody{
+		ResourceType: consts.RESOURCE_TYPE_PROJECT,
+		Members:      []membership.Member{member},
+	}
+	projects := []string{
+		// add project or container IDs here
+		"123-456-789",
+	}
+	for _, p := range projects {
+		res, err := c.Membership.Membership.AddMembersWithResponse(ctx, p, body)
+		if agg := validate.Response(res, err); agg != nil {
+			fmt.Println(agg)
+			continue
+		}
+		fmt.Printf("added to project %s\n", p)
+	}
+}
