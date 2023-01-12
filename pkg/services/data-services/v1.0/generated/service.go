4bbd683dce268ccc4bea7e190639126002ce6f2a
Dean <deanoren@gmail.com>
2023-01-12 16:48:40 +0100

    Fix/postgres url (#102)
    * fix dsa postgres url
    
    * re-generate
    
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

--- pkg/services/data-services/v1.0/generated/service.go
+++ pkg/services/data-services/v1.0/generated/service.go
@@ -20,13 +20,14 @@ func NewService(c common.Client, serviceID int) *ClientWithResponses {
 
 	switch serviceID {
 	case ElasticSearch:
+
 		url = "https://elasticsearch.api.eu01.stackit.cloud"
 	case LogMe:
 		url = "https://logme.api.eu01.stackit.cloud"
 	case MariaDB:
 		url = "https://mariadb.api.eu01.stackit.cloud"
 	case PostgresDB:
-		url = "https://postgres.api.eu01.stackit.cloud"
+		url = "https://postgresql.api.eu01.stackit.cloud"
 	case RabbitMQ:
 		url = "https://rabbitmq.api.eu01.stackit.cloud"
 	case Redis:
