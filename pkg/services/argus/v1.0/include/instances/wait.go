09775e484356efb739056648688b380cc88c9d71
Dean <deanoren@gmail.com>
2023-01-12 12:20:18 +0100

    retry connection reset by peer (#101)
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

--- pkg/services/argus/v1.0/include/instances/wait.go
+++ pkg/services/argus/v1.0/include/instances/wait.go
@@ -12,12 +12,19 @@ import (
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/wait"
 )
 
+const (
+	connection_reset = "read: connection reset" // catch connection reset by peer error
+)
+
 // WaitHandler will wait for instance creation
 // returned interface is nil or *instances.ProjectInstanceUI
 func (r InstanceCreateResponse) WaitHandler(ctx context.Context, c *instances.ClientWithResponses, projectID, instanceID string) *wait.Handler {
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.InstanceReadWithResponse(ctx, projectID, instanceID)
 		if err != nil {
+			if strings.Contains(err.Error(), connection_reset) {
+				return nil, false, nil
+			}
 			return nil, false, err
 		}
 		if s.StatusCode() == http.StatusInternalServerError {
@@ -46,6 +53,9 @@ func (r InstanceUpdateResponse) WaitHandler(ctx context.Context, c *instances.Cl
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.InstanceReadWithResponse(ctx, projectID, instanceID)
 		if err != nil {
+			if strings.Contains(err.Error(), connection_reset) {
+				return nil, false, nil
+			}
 			return nil, false, err
 		}
 		if s.StatusCode() == http.StatusInternalServerError {
@@ -104,8 +114,8 @@ func (r InstanceDeleteResponse) WaitHandler(ctx context.Context, c *instances.Cl
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.InstanceReadWithResponse(ctx, projectID, instanceID)
 		if err != nil {
-			if strings.Contains(err.Error(), http.StatusText(http.StatusNotFound)) {
-				return nil, true, nil
+			if strings.Contains(err.Error(), connection_reset) {
+				return nil, false, nil
 			}
 			return nil, false, err
 		}
