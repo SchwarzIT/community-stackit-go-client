32b606379c7c099d692edd04fc30746cbe642e3e
Dean Oren <deangili.oren@mail.schwarz>
2023-01-27 12:20:07 +0100

    add argus base urls

--- pkg/services/argus/v1.0/generated/service.go
+++ pkg/services/argus/v1.0/generated/service.go
@@ -2,20 +2,17 @@ package argus
 
 import (
 	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/urls"
+)
+
+var BaseURLs = urls.Init(
+	"argus",
+	"https://argus.api.stackit.cloud",
+	"https://argus.api.stg.stackit.cloud",
+	"https://argus.api.dev.stackit.cloud",
 )
 
 func NewService(c common.Client) *ClientWithResponses {
-	nc, _ := NewClientWithResponses(getURL(c), WithHTTPClient(c))
+	nc, _ := NewClientWithResponses(BaseURLs.GetURL(c), WithHTTPClient(c))
 	return nc
-}
-
-func getURL(c common.Client) string {
-	switch c.GetEnvironment() {
-	case common.ENV_DEV:
-		return "https://argus.api.dev.stackit.cloud"
-	case common.ENV_QA:
-		return "https://argus.api.stg.stackit.cloud"
-	default:
-		return "https://argus.api.stackit.cloud"
-	}
 }
