c8b5ab1d0e0bd0c5b8ebc6577ca74f6ab1dcc977
Dean <deanoren@gmail.com>
2023-02-10 10:32:07 +0100

    fix versions (#125)
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

--- pkg/services/mongodb-flex/v1.0/generated/versions/versions.go
+++ pkg/services/mongodb-flex/v1.0/generated/versions/versions.go
@@ -5,10 +5,36 @@ package versions
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/pkg/errors"
 
 	common "github.com/SchwarzIT/community-stackit-go-client/internal/common"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/validate"
+	"github.com/do87/oapi-codegen/pkg/runtime"
 )
+
+const (
+	BearerAuthScopes = "BearerAuth.Scopes"
+)
+
+// InstanceError defines model for instance.Error.
+type InstanceError struct {
+	Code    *int                 `json:"code,omitempty"`
+	Fields  *map[string][]string `json:"fields,omitempty"`
+	Message *string              `json:"message,omitempty"`
+	Type    *string              `json:"type,omitempty"`
+}
+
+// InstanceGetVersionsResponse defines model for instance.GetVersionsResponse.
+type InstanceGetVersionsResponse struct {
+	Versions *[]string `json:"versions,omitempty"`
+}
 
 // RequestEditorFn  is the function signature for the RequestEditor callback function
 type RequestEditorFn func(ctx context.Context, req *http.Request) error
@@ -38,6 +64,54 @@ func NewClient(server string, httpClient common.Client) *Client {
 
 // The interface specification for the client above.
 type ClientInterface interface {
+	// GetVersions request
+	GetVersions(ctx context.Context, projectID string, reqEditors ...RequestEditorFn) (*http.Response, error)
+}
+
+func (c *Client) GetVersions(ctx context.Context, projectID string, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewGetVersionsRequest(ctx, c.Server, projectID)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+// NewGetVersionsRequest generates requests for GetVersions
+func NewGetVersionsRequest(ctx context.Context, server string, projectID string) (*http.Request, error) {
+	var err error
+
+	var pathParam0 string
+
+	pathParam0, err = runtime.StyleParamWithLocation("simple", false, "projectID", runtime.ParamLocationPath, projectID)
+	if err != nil {
+		return nil, err
+	}
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/projects/%s/versions", pathParam0)
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", queryURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
 }
 
 func (c *Client) applyEditors(ctx context.Context, req *http.Request, additionalEditors []RequestEditorFn) error {
@@ -62,4 +136,73 @@ func NewClientWithResponses(server string, httpClient common.Client) *ClientWith
 
 // ClientWithResponsesInterface is the interface specification for the client with responses above.
 type ClientWithResponsesInterface interface {
+	// GetVersions request
+	GetVersionsWithResponse(ctx context.Context, projectID string, reqEditors ...RequestEditorFn) (*GetVersionsResponse, error)
+}
+
+type GetVersionsResponse struct {
+	Body         []byte
+	HTTPResponse *http.Response
+	JSON200      *InstanceGetVersionsResponse
+	JSON400      *InstanceError
+	HasError     error // Aggregated error
+}
+
+// Status returns HTTPResponse.Status
+func (r GetVersionsResponse) Status() string {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.Status
+	}
+	return http.StatusText(0)
+}
+
+// StatusCode returns HTTPResponse.StatusCode
+func (r GetVersionsResponse) StatusCode() int {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.StatusCode
+	}
+	return 0
+}
+
+// GetVersionsWithResponse request returning *GetVersionsResponse
+func (c *ClientWithResponses) GetVersionsWithResponse(ctx context.Context, projectID string, reqEditors ...RequestEditorFn) (*GetVersionsResponse, error) {
+	rsp, err := c.GetVersions(ctx, projectID, reqEditors...)
+	if err != nil {
+		return nil, err
+	}
+	return c.ParseGetVersionsResponse(rsp)
+}
+
+// ParseGetVersionsResponse parses an HTTP response from a GetVersionsWithResponse call
+func (c *ClientWithResponses) ParseGetVersionsResponse(rsp *http.Response) (*GetVersionsResponse, error) {
+	bodyBytes, err := io.ReadAll(rsp.Body)
+	defer func() { _ = rsp.Body.Close() }()
+	if err != nil {
+		return nil, err
+	}
+
+	response := &GetVersionsResponse{
+		Body:         bodyBytes,
+		HTTPResponse: rsp,
+	}
+	response.HasError = validate.DefaultResponseErrorHandler(rsp)
+
+	switch {
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 200:
+		var dest InstanceGetVersionsResponse
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, errors.Wrap(err, fmt.Sprintf("body was: %s", string(bodyBytes)))
+		}
+		response.JSON200 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 400:
+		var dest InstanceError
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, errors.Wrap(err, fmt.Sprintf("body was: %s", string(bodyBytes)))
+		}
+		response.JSON400 = &dest
+
+	}
+
+	return response, nil
 }
