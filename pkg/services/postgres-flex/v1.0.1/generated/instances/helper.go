1d99f606550f2d03ce760618a5e0adaa8ad4cf1c
Dean <deanoren@gmail.com>
2023-02-14 15:58:49 +0100

    Postgres flex v1.0.1 (#131)
    * add postgres flex v1.0.1
    
    * modify credntials and offerings
    
    ---------
    
    Co-authored-by: Dean Oren <deangili.oren@mail.schwarz>

---
+++ pkg/services/postgres-flex/v1.0.1/generated/instances/helper.go
@@ -0,0 +1,9 @@
+package instances
+
+const (
+	// Instance status options
+	STATUS_READY      = "READY"
+	STATUS_FAILED     = "FAILED"
+	STATUS_PROCESSING = "PROCESSING"
+	STATUS_UNKNOWN    = "UNKNOWN"
+)
---
+++ pkg/services/postgres-flex/v1.0.1/include/service.go
@@ -0,0 +1,22 @@
+package postgresflex
+
+import (
+	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
+	postgresflex "github.com/SchwarzIT/community-stackit-go-client/pkg/services/postgres-flex/v1.0.1/generated"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/urls"
+)
+
+var BaseURLs = urls.Init(
+	"postgres_flex",
+	"https://postgres-flex-service.api.eu01.stackit.cloud",
+	"https://postgres-flex-service.api.eu01.qa.stackit.cloud",
+	"https://postgres-flex-service.api.eu01.dev.stackit.cloud",
+)
+
+func NewService(c common.Client) *postgresflex.ClientWithResponses {
+	nc, _ := postgresflex.NewClientWithResponses(
+		BaseURLs.GetURL(c),
+		postgresflex.WithHTTPClient(c),
+	)
+	return nc
+}
