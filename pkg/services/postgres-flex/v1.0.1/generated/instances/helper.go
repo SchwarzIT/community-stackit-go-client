3581f46e13c8c241b3daafe9bd8e6617425bf4e6
Dean <deanoren@gmail.com>
2023-02-14 16:27:10 +0100

    Revert "Postgres flex v1.0.1 (#131)"
    This reverts commit 1d99f606550f2d03ce760618a5e0adaa8ad4cf1c.

--- pkg/services/postgres-flex/v1.0.1/generated/instances/helper.go
+++
@@ -1,9 +0,0 @@
-package instances
-
-const (
-	// Instance status options
-	STATUS_READY      = "READY"
-	STATUS_FAILED     = "FAILED"
-	STATUS_PROCESSING = "PROCESSING"
-	STATUS_UNKNOWN    = "UNKNOWN"
-)
