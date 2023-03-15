882d9c533456f14114737847855476a1f3880a44
Dean Oren <deangili.oren@mail.schwarz>
2023-03-15 14:07:18 +0100

    add costs v2

---
+++ pkg/services/costs/v2.0/generated/service.go
@@ -0,0 +1,18 @@
+package costs
+
+import (
+	"github.com/SchwarzIT/community-stackit-go-client/internal/common"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/urls"
+)
+
+var BaseURLs = urls.Init(
+	"costs",
+	"https://api.stackit.cloud/costs-service/v2/",
+	"https://api-qa.stackit.cloud/costs-service/v2/",
+	"https://api-dev.stackit.cloud/costs-service/v2/",
+)
+
+func NewService(c common.Client) *ClientWithResponses {
+	s, _ := NewClientWithResponses(BaseURLs.GetURL(c), WithHTTPClient(c))
+	return s
+}
