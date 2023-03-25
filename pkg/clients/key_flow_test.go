985a719d991bc25197527e5de67693c462d6e35e
Dean Oren <deangili.oren@mail.schwarz>
2023-03-25 14:55:26 +0100

    fix test

--- pkg/clients/key_flow_test.go
+++ pkg/clients/key_flow_test.go
@@ -14,9 +14,13 @@ func TestKeyFlow_processConfig(t *testing.T) {
 	a := os.Getenv(PrivateKeyPath)
 	b := os.Getenv(ServiceAccountKeyPath)
 	c := os.Getenv(Environment)
+	d := os.Getenv(PrivateKey)
+	e := os.Getenv(ServiceAccountKey)
 
 	os.Setenv(PrivateKeyPath, "test 1")
 	os.Setenv(ServiceAccountKeyPath, "test 2")
+	os.Setenv(PrivateKey, "test 3")
+	os.Setenv(ServiceAccountKey, "test 4")
 	os.Setenv(Environment, "dev")
 
 	kf := &KeyFlow{}
@@ -24,9 +28,9 @@ func TestKeyFlow_processConfig(t *testing.T) {
 
 	want := KeyFlowConfig{
 		PrivateKeyPath:        "test 1",
-		PrivateKey:            []byte{},
 		ServiceAccountKeyPath: "test 2",
-		ServiceAccountKey:     []byte{},
+		PrivateKey:            []byte("test 3"),
+		ServiceAccountKey:     []byte("test 4"),
 		Environment:           env.Parse("dev"),
 	}
 	assert.EqualValues(t, want, *kf.config)
@@ -35,6 +39,8 @@ func TestKeyFlow_processConfig(t *testing.T) {
 	os.Setenv(PrivateKeyPath, a)
 	os.Setenv(ServiceAccountKeyPath, b)
 	os.Setenv(Environment, c)
+	os.Setenv(PrivateKey, d)
+	os.Setenv(ServiceAccountKey, e)
 
 	type args struct {
 		cfg []KeyFlowConfig
@@ -46,6 +52,8 @@ func TestKeyFlow_processConfig(t *testing.T) {
 		{"test manual 1", args{[]KeyFlowConfig{
 			{PrivateKeyPath: "test 1"},
 			{ServiceAccountKeyPath: "test 2"},
+			{PrivateKey: []byte("test 3")},
+			{ServiceAccountKey: []byte("test 4")},
 			{Environment: "dev"},
 		}}},
 	}
