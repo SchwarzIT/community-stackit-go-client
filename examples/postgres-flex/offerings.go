6ec7614b1b7eec5b10dee4f21e0037c15ff390d8
Dean Oren <deangili.oren@mail.schwarz>
2023-03-25 10:29:03 +0100

    update postgres flex

---
+++ examples/postgres-flex/offerings.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	stackit "github.com/SchwarzIT/community-stackit-go-client"
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/validate"
+)
+
+func main() {
+	ctx := context.Background()
+	c := stackit.MustNewClientWithTokenAuth(ctx)
+
+	res, err := c.ElasticSearch.Offerings.Get(ctx, "my-project-id")
+	if err = validate.Response(res, err, "JSON200"); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Received the following offerings:")
+	for _, o := range res.JSON200.Offerings {
+		fmt.Printf("- %s\n", o.Name)
+	}
+}
