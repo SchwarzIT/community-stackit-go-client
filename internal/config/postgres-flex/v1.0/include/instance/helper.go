68f5babd889325a77fa2dc41bdd98e9435556bef
Dean Oren <deangili.oren@mail.schwarz>
2023-04-04 16:45:54 +0200

    modify wait helper

--- internal/config/postgres-flex/v1.0/include/instance/helper.go
+++ internal/config/postgres-flex/v1.0/include/instance/helper.go
@@ -2,7 +2,7 @@ package instance
 
 const (
 	// Instance status options
-	STATUS_READY      = "READY"
-	STATUS_FAILED     = "FAILED"
-	STATUS_PROCESSING = "PROCESSING"
+	STATUS_READY      = "Ready"
+	STATUS_FAILED     = "Failure"
+	STATUS_PROCESSING = "Progressing"
 )
--- pkg/services/postgres-flex/v1.0/instance/wait.go
+++ pkg/services/postgres-flex/v1.0/instance/wait.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/SchwarzIT/community-stackit-go-client/pkg/validate"
 	"github.com/SchwarzIT/community-stackit-go-client/pkg/wait"
 )
 
@@ -30,14 +31,8 @@ func waitForCreateOrUpdate(ctx context.Context, c *ClientWithResponses, projectI
 	time.Sleep(5 * time.Second)
 	return wait.New(func() (res interface{}, done bool, err error) {
 		s, err := c.Get(ctx, projectID, instanceID)
-		if err != nil {
+		if err = validate.Response(s, err, "JSON200.Item"); err != nil {
 			return nil, false, err
-		}
-		if s.Error != nil {
-			return nil, false, err
-		}
-		if s.JSON200 == nil || s.JSON200.Item == nil {
-			return nil, false, errors.New("bad response")
 		}
 		if *s.JSON200.Item.Status == STATUS_READY {
 			return s.JSON200.Item, true, nil
@@ -54,14 +49,11 @@ func waitForCreateOrUpdate(ctx context.Context, c *ClientWithResponses, projectI
 func (DeleteResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, instanceID string) *wait.Handler {
 	return wait.New(func() (interface{}, bool, error) {
 		res, err := c.Get(ctx, projectID, instanceID)
-		if err != nil {
+		if err = validate.Response(res, err); err != nil {
+			if res != nil && res.StatusCode() == http.StatusNotFound {
+				return nil, true, nil
+			}
 			return nil, false, err
-		}
-		if res.StatusCode() == http.StatusNotFound {
-			return nil, true, nil
-		}
-		if res.Error != nil {
-			return nil, false, res.Error
 		}
 		return nil, false, nil
 	})
