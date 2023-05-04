5bff1dfc1946f91c9246b4f307b49f84aec2028e
Dean Oren <deangili.oren@mail.schwarz>
2023-05-04 08:48:11 +0200

    update clients and flows

--- pkg/helpers/traceparent/traceparent_test.go
+++ pkg/helpers/traceparent/traceparent_test.go
@@ -1,6 +1,7 @@
 package traceparent
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,11 +12,11 @@ func TestGenerate(t *testing.T) {
 	assert.NoError(t, err)
 
 	// traceparent should have 55 characters: 2 for version, 32 for traceID, 16 for spanID, 2 for flag, and 3 hyphens
-	assert.Equal(t, 55, len(tp))
+	assert.Equal(t, 55, len(tp.String()))
 
 	// since Generate uses RecordFlag and CurrentVersion, we expect those to be in the traceparent
-	assert.Contains(t, tp, string(CurrentVersion))
-	assert.Contains(t, tp, string(RecordFlag))
+	assert.Contains(t, tp.Version, string(CurrentVersion))
+	assert.Contains(t, tp.Flag, string(RecordFlag))
 }
 
 func TestGenerateCustom(t *testing.T) {
@@ -23,9 +24,30 @@ func TestGenerateCustom(t *testing.T) {
 	assert.NoError(t, err)
 
 	// traceparent should have 55 characters: 2 for version, 32 for traceID, 16 for spanID, 2 for flag, and 3 hyphens
-	assert.Equal(t, 55, len(tp))
+	assert.Equal(t, 55, len(tp.String()))
 
 	// since GenerateCustom uses the provided flag and version, we expect those to be in the traceparent
-	assert.Contains(t, tp, string(DoNotRecordFlag))
-	assert.Contains(t, tp, "01")
+	assert.Contains(t, tp.Flag, string(DoNotRecordFlag))
+	assert.Contains(t, tp.Version, "01")
+}
+
+func TestSetHeader(t *testing.T) {
+	tp, _ := Generate()
+
+	// Create a new http.Request
+	req, err := http.NewRequest("GET", "http://test.io", nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	// Call SetHeader with the Traceparent
+	tp.SetHeader(req)
+
+	// Check if the "Traceparent" header was set correctly
+	assert.Equal(t, tp.String(), req.Header.Get("Traceparent"))
+
+	// Test with a nil http.Request
+	var nilReq *http.Request
+	tp.SetHeader(nilReq)
+	assert.Nil(t, nilReq)
 }
