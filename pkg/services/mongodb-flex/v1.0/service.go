b5730b4f4d094c01474f78b6227641266d9ba42d
Dean Oren <deangili.oren@mail.schwarz>
2023-07-14 14:33:30 +0200

    update flex service

--- pkg/services/mongodb-flex/v1.0/service.go
+++ pkg/services/mongodb-flex/v1.0/service.go
@@ -7,9 +7,9 @@ import (
 
 var BaseURLs = env.URLs(
 	"mongodb_flex",
-	"https://mongodb-flex-service.api.eu01.stackit.cloud",
-	"https://mongodb-flex-service.api.eu01.qa.stackit.cloud",
-	"https://mongodb-flex-service.api.eu01.dev.stackit.cloud",
+	"https://mongodb-flex-service.api.eu01.stackit.cloud/v1/",
+	"https://mongodb-flex-service.api.eu01.qa.stackit.cloud/v1/",
+	"https://mongodb-flex-service.api.eu01.dev.stackit.cloud/v1/",
 )
 
 func NewService(c contracts.BaseClientInterface) *ClientWithResponses {
