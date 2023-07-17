87ac19b59da5203a3aaab3b2915472c1e1b1d465
Dean Oren <deangili.oren@mail.schwarz>
2023-07-17 13:33:43 +0200

    add EnableTraceparent to config

--- pkg/contracts/client.go
+++ pkg/contracts/client.go
@@ -22,6 +22,5 @@ type ClientInterface[f ClientFlowConfig] interface {
 type BaseClientInterface interface {
 	Do(req *http.Request) (*http.Response, error)
 	GetServiceAccountEmail() string
-	EnableTraceparent()
 	Clone() interface{}
 }
