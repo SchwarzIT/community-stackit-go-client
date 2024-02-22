a8ae582cc6fa8b7a9bd9075bca9eddca7bc16593
Emil Georgiev <emil.georgiev@mail.schwarz>
2024-02-22 15:38:28 +0200

    ODJ-2783: add wait handler method for the type V1DeleteNetworkResponse

--- pkg/services/iaas-api/v1alpha/wait.go
+++ pkg/services/iaas-api/v1alpha/wait.go
@@ -32,3 +32,29 @@ func (*V1CreateNetworkResponse) WaitHandler(ctx context.Context, c *ClientWithRe
 		return resp, true, nil
 	})
 }
+
+func (*V1DeleteNetworkResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, networkID openapiTypes.UUID) *wait.Handler {
+	return wait.New(func() (res interface{}, done bool, err error) {
+		resp, err := c.V1GetNetwork(ctx, projectID, networkID)
+		if err != nil {
+			if strings.Contains(err.Error(), http.StatusText(http.StatusForbidden)) {
+				return nil, false, nil
+			}
+			return nil, false, err
+		}
+		if resp.StatusCode() == http.StatusForbidden {
+			return nil, false, nil
+		}
+		if resp.StatusCode() == http.StatusInternalServerError {
+			return nil, false, nil
+		}
+		if resp.StatusCode() == http.StatusNotFound {
+			return nil, false, nil
+		}
+		if resp.JSON200 == nil {
+			return nil, false, nil
+		}
+
+		return resp, true, nil
+	})
+}
