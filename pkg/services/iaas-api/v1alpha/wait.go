5f52368f4886d1d4c9a9131352f0f6922ae20760
Emil Georgiev <emil.georgiev@mail.schwarz>
2024-02-22 18:01:36 +0200

    Feature/odj 2783 add validation functions for resource network (#204)
    * ODJ-2783: add validation functions for the type Network
    
    * ODJ-2783: add wait handler method for the type V1DeleteNetworkResponse
    
    * ODJ-2783: add unit tests for the validation functions
    
    * ODJ-2783: add valdiation function for network ID

--- pkg/services/iaas-api/v1alpha/wait.go
+++ pkg/services/iaas-api/v1alpha/wait.go
@@ -10,7 +10,30 @@ import (
 	openapiTypes "github.com/SchwarzIT/community-stackit-go-client/pkg/helpers/types"
 )
 
-func (*V1CreateNetworkResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, networkID openapiTypes.UUID) *wait.Handler {
+func (*V1CreateNetworkResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID openapiTypes.UUID) *wait.Handler {
+	return wait.New(func() (res interface{}, done bool, err error) {
+		resp, err := c.V1ListNetworksInProject(ctx, projectID)
+		if err != nil {
+			if strings.Contains(err.Error(), http.StatusText(http.StatusForbidden)) {
+				return nil, false, nil
+			}
+			return nil, false, err
+		}
+		if resp.StatusCode() == http.StatusForbidden {
+			return nil, false, nil
+		}
+		if resp.StatusCode() == http.StatusInternalServerError {
+			return nil, false, nil
+		}
+		if resp.JSON200 == nil {
+			return nil, false, nil
+		}
+
+		return resp, true, nil
+	})
+}
+
+func (*V1DeleteNetworkResponse) WaitHandler(ctx context.Context, c *ClientWithResponses, projectID, networkID openapiTypes.UUID) *wait.Handler {
 	return wait.New(func() (res interface{}, done bool, err error) {
 		resp, err := c.V1GetNetwork(ctx, projectID, networkID)
 		if err != nil {
@@ -23,6 +46,9 @@ func (*V1CreateNetworkResponse) WaitHandler(ctx context.Context, c *ClientWithRe
 			return nil, false, nil
 		}
 		if resp.StatusCode() == http.StatusInternalServerError {
+			return nil, false, nil
+		}
+		if resp.StatusCode() == http.StatusNotFound {
 			return nil, false, nil
 		}
 		if resp.JSON200 == nil {
